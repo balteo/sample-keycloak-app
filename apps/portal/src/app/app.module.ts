03d8491754bfc7ec92464aba9b32faedfc2215e2
martin.julien <martin.julien@cn.medecin.fr>
2022-01-18 12:50:09 +0100

    WIP implement list categories

--- apps/portal/src/app/app.module.ts
+++ apps/portal/src/app/app.module.ts
@@ -8,20 +8,28 @@ import {MatCardModule} from "@angular/material/card";
 import {CategoryItemComponent} from './category/components/category-item/category-item.component';
 import {MatButtonModule} from "@angular/material/button";
 import {CategoryPageComponent} from './category/pages/category-page/category-page.component';
+import {MatIconModule} from "@angular/material/icon";
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatListModule} from "@angular/material/list";
 
 @NgModule({
   declarations: [AppComponent, CategoryListComponent, CategoryItemComponent, CategoryPageComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     MatCardModule,
     MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
     RouterModule.forRoot([
       {
         component: CategoryPageComponent,
         path: 'categories'
       }
     ], {initialNavigation: 'enabledBlocking'}),
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
