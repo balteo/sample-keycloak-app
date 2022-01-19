08e6ef4d1b5acda770d32d7bcfb12c43dd993447
martin.julien <martin.julien@cn.medecin.fr>
2022-01-19 09:06:36 +0100

    Refactor service

--- apps/portal/src/app/category/services/category.service.ts
+++ apps/portal/src/app/category/services/category.service.ts
@@ -8,26 +8,28 @@ import {Category} from "../models/category.model";
 })
 export class CategoryService {
 
+  readonly apiPrefix = '/api';
+
   constructor(private http: HttpClient) {
   }
 
   findCategories(): Observable<Category[]> {
-    return this.http.get<Category[]>('http://localhost:3000/category');
+    return this.http.get<Category[]>(`${this.apiPrefix}/category`);
   }
 
   createCategory(category: Category): Observable<unknown> {
-    return this.http.post('http://localhost:3000/category', category);
+    return this.http.post(`${this.apiPrefix}/category`, category);
   }
 
   updateCategory(category: Category): Observable<unknown> {
-    return this.http.put('http://localhost:3000/category/' + category.id, category);
+    return this.http.put(`${this.apiPrefix}/category/${category.id}`, category);
   }
 
   deleteCategory(categoryId: number): Observable<unknown> {
-    return this.http.delete('http://localhost:3000/category/' + categoryId);
+    return this.http.delete(`${this.apiPrefix}/category/${categoryId}`);
   }
 
   findCategoryById(categoryId: number): Observable<Category> {
-    return this.http.get<Category>('http://localhost:3000/category/' + categoryId);
+    return this.http.get<Category>(`${this.apiPrefix}/category/${categoryId}`);
   }
 }
