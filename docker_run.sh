80cb22e6e89a8e0f949d826ea22cf8691e3b0200
martin.julien <martin.julien@cn.medecin.fr>
2022-01-24 09:11:05 +0100

    WIP: dockerize test environment 4 (polish)

--- docker_run.sh
+++ docker_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name json-server -v $PWD/apps/portal/db.json:/db.json --rm -d -p 80:80 custom-json-server:latest
+docker run --name json-server --rm -d -p 80:80 custom-json-server:latest
